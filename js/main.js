d063994bf949b765ffd912a56186c11b6cac10c7
asimshrestha2 <asims178@gmail.com>
2016-10-28 17:04:25 -0400

    Portfolio v2

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var btnNames = [["Info", "./imgs/"], ["Work", "./imgs/"], ["Contact", "./imgs/"]];
+var current = 0;
+var previous = 0;
+window.onload = function() {
+  var panels = document.getElementsByClassName('panel');
+  var control = document.getElementById('control');
+  for (var i = 0; i < panels.length; i++) {
+    panels[i].arry_id = i;
+    control.innerHTML += '<div class="btn" style="background: url(\''+btnNames[i][1]+'\'); background-size: cover;">'+btnNames[i][0]+'</div>';
+  }
+
+  var btns = document.getElementsByClassName('btn');
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].arry_id = i;
+    btns[i].addEventListener('click', function(e){
+      var i = e.target.arry_id;
+      //console.log(btnNames[i][1]);
+      //console.log(i);
+      previous = current;
+      if (!panels[i].className.includes('current')) {
+        panels[i].classList.add('animate-in');
+        panels[i].classList.add('current');
+        if(current < panels.length){
+          panels[current].classList.add('animate-out');
+          panels[current].classList.remove('current');
+        }
+        setTimeout(function () {
+          for (var i = 0; i < panels.length; i++) {
+            if (panels[i].className.includes('animate-in')) {
+              panels[i].classList.remove('animate-in');
+            }
+
+            if (panels[i].className.includes('animate-out')) {
+              panels[i].classList.remove('animate-out');
+            }
+          }
+
+        }, 375);
+        current = i;
+      }
+    }, false);
+  }
+}
